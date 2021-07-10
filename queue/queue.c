f078efa68043c3fa5780e839fc22dec1e9df7126
Habibur Rahman <habibur.rahman0927@gmail.com>
2021-07-10 13:54:52 -0700

    data structure algorithm program practice file

---
+++ queue/queue.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<stdbool.h>
+
+#define CAPACITY 5
+int ourQueue[CAPACITY];
+int frot = 0, rear = -1, totalItem = 0;
+
+void enqueue(int item){
+
+
+}
+int dequeue(){
+
+    return 0;
+}
+
+int main()
+{
+    printf("\n*****************\n let's implement our queue\n\n");
+}
